e57c2a208bc8b9f807b549e1355dda01100d865b
Og'abek Shomurotov <o.shomurotov@uic.group>
2023-12-01 13:52:59 +0500

    feat: new tips

--- TSSimplified/vite_with_ts/src/examples/index.ts
+++ TSSimplified/vite_with_ts/src/examples/index.ts
@@ -1,35 +1,34 @@
-console.log('Hi')
+console.log("Hi");
 // const arr: number[] = [1, 2, 3, 1, 4]
 // console.log(arr)
 // const arr2: [1, 2, 3] = [1, 2, 3]
 // console.log(arr2)
 
 // Generics
-const input = document.querySelector<HTMLInputElement>('.input')
-console.log(input?.value)
+const input = document.querySelector<HTMLInputElement>(".input");
+console.log(input?.value);
 
 function getSecond<ArrayType>(array: ArrayType[]): ArrayType {
-    return array[1]
+  return array[1];
 }
 
-const a: number[] = [1, 2, 3]
-const a2: Array<number> = [1, 2, 3] // create object with generics
-const b: string[] = ['hi', 'hello']
-const b1: Array<string> = ['hi', 'hello'] // create object with generics
+const a: number[] = [1, 2, 3];
+const a2: Array<number> = [1, 2, 3]; // create object with generics
+const b: string[] = ["hi", "hello"];
+const b1: Array<string> = ["hi", "hello"]; // create object with generics
 
-const retA = getSecond<number>(a)
-const retB = getSecond(b)
+const retA = getSecond<number>(a);
+const retB = getSecond(b);
 
-const A = new Set<string>()
-A.add("test")
+const A = new Set<string>();
+A.add("test");
 
-const B = new Map<string, number>()
-B.set("element", 5)
+const B = new Map<string, number>();
+B.set("element", 5);
 
 // Generic types automatic detect with default value
-const C = new Set(['AD'])
-const D = new Map([['AD', 5]])
-
+const C = new Set(["AD"]);
+const D = new Map([["AD", 5]]);
 
 // Generics use cases (brand new)
 /*type APIResponse<TData> = {
@@ -88,36 +87,41 @@ const Y: APIResponse<{ name: string }> = {
 //     isError: false
 // }
 
-
 type APIResponse<TData extends object = { status: number }> = {
-    data: TData
-    isError: boolean
-}
+  data: TData;
+  isError: boolean;
+};
 
 const X: APIResponse = {
-    data: {
-        status: 200
-    },
-    isError: false
-}
+  data: {
+    status: 200,
+  },
+  isError: false,
+};
 
 // Array to Object
 function arrayToObject<T>(array: [string, T][]) {
-    const obj: {
-        [index: string]: T
-    } = {}
+  const obj: {
+    [index: string]: T;
+  } = {};
 
-    array.forEach(([key, value]) => {
-        obj[key] = value
-    })
+  array.forEach(([key, value]) => {
+    obj[key] = value;
+  });
 
-    return obj
+  return obj;
 }
 
 const arr: [string, number | boolean][] = [
-    ['keyOne', 1],
-    ['keyTwo', 2],
-    ['keyThree', true],
-]
+  ["keyOne", 1],
+  ["keyTwo", 2],
+  ["keyThree", true],
+];
 
-const obj = arrayToObject(arr)
+const obj = arrayToObject(arr);
+
+function createArrayPair<T, K>(elem1: T, elem2: K): [T, K] {
+  return [elem1, elem2];
+}
+
+createArrayPair("hello", 24);
